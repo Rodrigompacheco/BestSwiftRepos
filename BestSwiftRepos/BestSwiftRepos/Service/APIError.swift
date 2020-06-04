5b1f307dbd23e301b4e43458b9f95ca46848933e
Rodrigo Pacheco <rodrigo.rp.pacheco@gmail.com>
2020-06-04 19:04:03 -0300

    APIError done

---
+++ BestSwiftRepos/BestSwiftRepos/Service/APIError.swift
@@ -0,0 +1,31 @@
+//
+//  APIError.swift
+//  BestSwiftRepos
+//
+//  Created by Rodrigo Pacheco on 04/06/20.
+//  Copyright © 2020 Rodrigo Pacheco. All rights reserved.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case custom(String)
+}
+
+extension APIError {
+    static var makeRequest: APIError {
+        return APIError.custom("Couldn't create request.")
+    }
+    
+    static var decode: APIError {
+        return APIError.custom("Couldn't decode data.")
+    }
+    
+    static var invalidURL: APIError {
+        return APIError.custom("Invalid URL.")
+    }
+    
+    static var invalidData: APIError {
+           return APIError.custom("Invalid Data.")
+    }
+}
