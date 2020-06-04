999f68c1931e14d4f81f89b717af922a64bf50a0
Rodrigo Pacheco <rodrigo.rp.pacheco@gmail.com>
2020-06-04 18:52:25 -0300

    Implemented the coordinator logic

---
+++ BestSwiftRepos/BestSwiftRepos/Coordinator/Coordinator.swift
@@ -0,0 +1,18 @@
+//
+//  Coordinator.swift
+//  BestSwiftRepos
+//
+//  Created by Rodrigo Pacheco on 04/06/20.
+//  Copyright © 2020 Rodrigo Pacheco. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+}
+
