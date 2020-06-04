3517c963e8d143b84dfb17fa614a94246b835eab
Rodrigo Pacheco <rodrigo.rp.pacheco@gmail.com>
2020-06-04 19:00:19 -0300

    Pod init

---
+++ BestSwiftRepos/Pods/Target Support Files/Pods-BestSwiftRepos/Pods-BestSwiftRepos-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BestSwiftRepos : NSObject
+@end
+@implementation PodsDummy_Pods_BestSwiftRepos
+@end
---
+++ BestSwiftRepos/Pods/Target Support Files/Pods-BestSwiftReposTests/Pods-BestSwiftReposTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BestSwiftReposTests : NSObject
+@end
+@implementation PodsDummy_Pods_BestSwiftReposTests
+@end
